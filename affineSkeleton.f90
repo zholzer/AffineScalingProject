d86fabec9dc1fedfcb80ec524ea1c1b8efc5eac6
zholzer <144373470+zholzer@users.noreply.github.com>
2023-12-03 21:21:54 -0800

    Update affineSkeleton.f90
    too many test cases

--- affineSkeleton.f90
+++ affineSkeleton.f90
@@ -23,20 +23,42 @@ program affineScaling
             b = reshape([15.0, 15.0], [1,2])
             c = reshape([-2.0, 1.0, 0.0, 0.0],[4,1])
             xk = reshape([10.0, 2.0, 7.0, 13.0],[4,1])
-            ! answer is (35 15 0 0)
+            ! answer is (30 15 0 0)
 
         case ('B')
             allocate(A(2,4))
             allocate(b(1,2))
             allocate(c(4,1))
             allocate(xk(4,1))
-            A = reshape([1.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 1.0], [2, 4]) 
-            b = reshape([1.0, 2.0], [1,2])
-            c = reshape([-1.0, -1.0, 1.0, 1.0],[4,1])
-            xk = reshape([1.0, 2.0, 0.0, 0.0],[4,1])
+            A = reshape([0.8, 0.0, .2, 0.0, 0.0, .1, 0.0, 1.9], [2, 4]) 
+            b = reshape([1.0, 2.0], [2,1])
+            c = reshape([-0.8, -0.1, .2, 1.9],[4,1])
+            xk = reshape([1, 1, 1, 1],[4,1])
             ! answer is (1 2 0 0)
-
+        
         case ('C')
+            allocate(A(2,4))
+            allocate(b(1,2))
+            allocate(c(4,1))
+            allocate(xk(4,1))
+            A = reshape([1.0, 1.0, 1.0, 0.0, 2.0, 1.0, 0.0, 1.0], [2, 4]) 
+            b = reshape([40.0, 60.0], [2,1])
+            c = reshape([1.0, -2.0, 0.0, 0.0],[4,1])
+            xk = reshape([15.0, 15.0, 10.0, 15.0],[4,1])
+        !    https://www.ise.ncsu.edu/fuzzy-neural/wp-content/uploads/sites/9/2021/10/Lecture-6.pdf
+
+        case ('D')
+            allocate(A(2,4))
+            allocate(b(1,2))
+            allocate(c(4,1))
+            allocate(xk(4,1))
+            A = reshape([1.0, 2.0, 1.0, 0.0, 2.0, 1.0, 0.0, 1.0], [2, 4]) 
+            b = reshape([3.0, 3.0], [2,1])
+            c = reshape([-1.0, -1.0, 2.44, 1.97],[4,1])
+            xk = reshape([.5, .03, 0.0, 0.0],[4,1])
+        !    https://www.ise.ncsu.edu/fuzzy-neural/wp-content/uploads/sites/9/2021/10/Lecture-6.pdf
+
+        case ('E')
             allocate(A(10,10))
             allocate(b(1,10))
             allocate(c(10,1))
@@ -54,29 +76,11 @@ program affineScaling
                 [10, 10]) 
                 b = reshape([1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0], [1,10])
                 c = reshape([1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],[10,1])
-                xk = reshape([1.0, 1.0/2.0, 1.0/3.0, 1.0/4.0, 1.0/5.0, 1.0/6.0, 1.0/7.0, 1.0/8.0, 1.0/9.0, 1.0/10.0],[10,1])
+                xk = reshape([1.0, .4, .2, .1, .1, .1, .1, .05, .05, .01],[10,1]) 
             ! answer is (1 0 ... 0), telescoping
 
-        case ('D')
-            allocate(A(8,3))
-            allocate(b(8,1))
-            allocate(c(3,1))
-            allocate(xk(3,1))
-            A = reshape([0.0, 1.0, 0.0, & 
-                1.0, 0.0, -1.0, &
-                0.0, 1.0, 0.0, &
-                1.0, 0.0, -1.0, &
-                0.0, 1.0, 0.0, &
-                1.0, 0.0, -1.0, &
-                0.0, 1.0, 0.0, &
-                1.0, 0.0, -1.0], & 
-                [8, 3]) 
-            b = reshape([1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0], [8,1])
-            c = reshape([1.0, 1.0, 1.0],[3,1])
-            xk = reshape([2.0, 1.0, 5.0],[3,1])
-            ! answer is (2 1 1)???
 
-        case ('E')
+        case ('F')
             allocate(A(10,10))
             allocate(b(1,10))
             allocate(c(10,1))
@@ -96,6 +100,26 @@ program affineScaling
             c = reshape([1.0, -1.0, 1.0, -1.0, 1.0, -1.0, 1.0, -1.0, 1.0, -1.0],[10,1])
             xk = reshape([1, 1/2, 1/3, 1/4, 1/5, 1/6, 1/7, 1/8, 1/9, 1/10],[10,1])
 
+        case ('G')
+            allocate(A(2,4))
+            allocate(b(1,2))
+            allocate(c(4,1))
+            allocate(xk(4,1))
+            A = reshape([1.0, 4.0, 3.0, 1.0, 1.0, 1.0], [3, 2]) 
+            b = reshape([24.0, 21.0, 9.0], [1,3])
+            c = reshape([2.0, 5.0],[2,1])
+            xk = reshape([.1, .1],[2,1])
+
+        case ('H')
+            allocate(A(2,2))
+            allocate(b(1,2))
+            allocate(c(2,1))
+            allocate(xk(2,1))
+            A = reshape([1.0, 1.0, 1.0, 0.0], [2, 2]) 
+            b = reshape([2.0, 1.0], [1,2])
+            c = reshape([-3.0, -1.0],[2,1])
+            xk = reshape([.6, .6],[2,1])
+
         case default
             write(*,*) 'Please enter a valid test case selection.'
             write(*,*) 'Please enter a test case as "A", "B", "C", "D", or "E": '
@@ -113,7 +137,7 @@ program affineScaling
         read(*,*, iostat=stat) stepSize
     end do
 
-    tolerance = .00001
+    tolerance = 10**(-9)
 
     allocate(D(size(xk),size(xk)))
     allocate(dk(size(xk),1))
@@ -123,7 +147,7 @@ program affineScaling
     allocate(oneVec(1,size(A, dim=2)))
 
     call cpu_time(start)
-    itNum = 10000
+    itNum = 1000
     do i = 1,itNum ! can change this later
         ! Step 1: Start an an interior feasible point
         !vk = b - A*xk
@@ -148,10 +172,11 @@ program affineScaling
         end do
 
         temp = MATMUL(oneVec,MATMUL(D,r))
+        
         if ((posR .and. (temp(1,1)) < tolerance)) then ! doesnt work
             write(*, *) 'Primal optimal value: ', xk 
             call cpu_time(finish)
-            write(*,*) 'Code took ', finish, 'seconds with ', i, ' iterations.'
+            write(*,*) 'Code took ', finish - start, 'seconds with ', i, ' iterations.'
             stop
         end if
 
@@ -170,7 +195,7 @@ program affineScaling
 
         zerodk = .true.;
         do j = 1,size(dk)
-            if (dk(j,1) .eq. 0) then
+            if (dk(j,1) <= tolerance) then
                 continue
             else
                 zerodk = .false.
@@ -178,24 +203,24 @@ program affineScaling
         end do
        
         ! Step 4: Check for unbounded and constant objective value
-        if (posdk) then 
-            write(*,*)"This problem is unbounded"
-            stop
-        else if (zerodk) then 
+        !if (posdk) then 
+            !write(*,*)"This problem is unbounded"
+            !stop
+         if (zerodk) then 
             write(*,*)"Primal Optimal value: ",xk
             call cpu_time(finish)
-            write(*,*) 'Code took ', finish, 'seconds with ', i, ' iterations.'
+            write(*,*) 'Code took ', finish - start, 'seconds with ', i, ' iterations.'
             stop
         end if
 
         ! ^ if the above conditions are unsatisfied, go to the next step
 
         ! Step 5: Perform the translation
-        call computeTranslation(xk, stepSize, D)
+        call computeTranslation(xk, stepSize, dk, D)
         if (i == itNum) then
             write(*,*) "Primal Optimal value: ", xk
             call cpu_time(finish)
-            write(*,*) 'Code took ', finish, 'seconds with ', itNum, ' iterations.'
+            write(*,*) 'Code took ', finish - start, 'seconds with ', itNum, ' iterations.'
             stop
         end if
     end do
@@ -244,16 +269,20 @@ program affineScaling
             real, dimension(:,:), intent(out) :: dk
         
             dk = MATMUL(-1*D, r)
-
+            !write(*,*) dk
         end subroutine computeSteepestDescentDirection
 
-        subroutine computeTranslation(xk, stepSize, D)
+        subroutine computeTranslation(xk, stepSize, dk, D)
             implicit none
-            real, dimension(:,:), intent(inout) :: xk
+            real, dimension(:,:), intent(inout) :: xk, dk
             real, intent(in) :: stepSize
+            real :: beta
             real, dimension(:,:), intent(in) :: D
 
-            xk = xk + stepSize*MATMUL(D,dk)
+            beta = stepSize/maxval(-dk)
+            xk = xk + beta*MATMUL(D,dk)
+            !write(*,*) beta
+            !xk = xk - stepSize*MATMUL(D**2,dk)/norm2(MATMUL(D,dk))
 
         end subroutine computeTranslation
 
