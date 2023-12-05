c7bc422656e33f34c72e701b311027b95fd597b5
zholzer <144373470+zholzer@users.noreply.github.com>
2023-12-04 22:19:33 -0800

    Update affineSkeleton.f90
    Clean!

--- affineSkeleton.f90
+++ affineSkeleton.f90
@@ -1,29 +1,30 @@
 program affineScaling
     implicit none
     ! Setup: Take input A, b, c, step size, tolerance ε, and initial guess x0>0
-    real, dimension(:,:), allocatable :: A, b, D, oneVec, c, xk, dk, r, vk, w ! switch cases?
-    real :: stepSize, tolerance, start, finish ! user input?
-    real, dimension(1,1) :: temp
+    real, dimension(:,:), allocatable :: A, b, D, oneVec, c, xk, dk, r, vk, w 
+    real :: stepSize, tolerance, start, finish
+    real, dimension(1,1) :: check
     integer :: i,j, itNum, stat
     logical :: posdk, zerodk, posR
     character :: testChoice
 
-    ! to start we can test with hard coding, maybe one from a known reference
-    ! using 7.1 from [6]
-    write(*,*) 'Please enter a test case as "A", "B", "C", "D", or "E": '
+    ! using 7.1 from https://www.ise.ncsu.edu/fuzzy-neural/wp-content/uploads/sites/9/2019/10/chapter7.pdf
+    write(*,*) 'Welcome to the Affine Scaling Program. Descriptions of the cases can be found in the README.txt file.'
+    write(*,'(a)', advance = 'no') 'Please enter a test case as "A", "B", "C", "D", or "E":     '
     read(*,*, iostat=stat) testChoice
 
- 1   select case (testChoice)
+ 1   select case (testChoice) ! switches with user input
         case ('A')
-            allocate(A(2,4))
+            allocate(A(2,4)) ! allocate space depending on choice
             allocate(b(1,2))
             allocate(c(4,1))
             allocate(xk(4,1))
-            A = reshape([1.0, 0.0, -1.0, 1.0, 1.0, 0.0, 0.0, 1.0], [2, 4]) 
+            A = reshape([1.0, 0.0, -1.0, 1.0, 1.0, 0.0, 0.0, 1.0], [2, 4])  ! fill in problem vectors
             b = reshape([15.0, 15.0], [1,2])
             c = reshape([-2.0, 1.0, 0.0, 0.0],[4,1])
             xk = reshape([10.0, 2.0, 7.0, 13.0],[4,1])
             ! answer is (30 15 0 0)
+            ! reference https://www.ise.ncsu.edu/fuzzy-neural/wp-content/uploads/sites/9/2019/10/chapter7.pdf
 
         case ('B')
             allocate(A(2,4))
@@ -35,7 +36,7 @@ program affineScaling
             c = reshape([-1.0, -1.0, 2.0, 1.0],[4,1])
             xk = reshape([0.9, 1.9, 0.1, 0.1],[4,1])
             ! answer is (1 2 0 0)
-        ! https://homepages.rpi.edu/~mitchj/handouts/interior_html/interior.html
+        ! reference https://homepages.rpi.edu/~mitchj/handouts/interior_html/interior.html
         
         case ('C')
             allocate(A(2,4))
@@ -46,8 +47,8 @@ program affineScaling
             b = reshape([40.0, 60.0], [1,2])
             c = reshape([1.0, -2.0, 0.0, 0.0],[4,1])
             xk = reshape([8.0, 30.0, 2.0, 14.0],[4,1])
-            !Answer is (0 40 0 20) maybe?
-        !    https://www.ise.ncsu.edu/fuzzy-neural/wp-content/uploads/sites/9/2021/10/Lecture-6.pdf
+            ! Answer is (0 40 0 20) 
+        ! reference https://www.ise.ncsu.edu/fuzzy-neural/wp-content/uploads/sites/9/2021/10/Lecture-6.pdf
 
         case ('D')
             allocate(A(2,4))
@@ -58,53 +59,10 @@ program affineScaling
             b = reshape([3.0, 3.0], [2,1])
             c = reshape([-1.0, -1.0, 0.0, 0.0],[4,1])
             xk = reshape([0.9, 0.9, 0.3, 0.3],[4,1])
-                 ! answer is? (1 1 ? ?), beta = .5
-            ! getting (2.7 2.7 0 0)???
+                 ! (1 1 ? ?), beta = .5
         ! reference https://juliabook.chkwon.net/book/interior
 
-        case ('E')
-            allocate(A(10,10))
-            allocate(b(1,10))
-            allocate(c(10,1))
-            allocate(xk(10,1))
-            A = reshape([1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, & 
-                0.0, 2.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, &
-                0.0, 0.0, 3.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, &
-                0.0, 0.0, 0.0, 4.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, &
-                0.0, 0.0, 0.0, 0.0, 5.0, 0.0, 0.0, 0.0, 0.0, 0.0, &
-                0.0, 0.0, 0.0, 0.0, 0.0, 6.0, 0.0, 0.0, 0.0, 0.0, &
-                0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 7.0, 0.0, 0.0, 0.0, &
-                0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 8.0, 0.0, 0.0, &
-                0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 9.0, 0.0, &
-                0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 10.0], &    
-                [10, 10]) 
-                b = reshape([1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0], [1,10])
-                c = reshape([1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],[10,1])
-                xk = reshape([1.0, 1.0/2.0, 1.0/3.0, 1.0/4.0, 1.0/5.0, 1.0/6.0, 1.0/7.0, 1.0/8.0, 1.0/9.0, 1.0/10.0],[10,1])
-            ! answer is (1 0 ... 0), telescoping
-
-
-        case ('F')
-            allocate(A(10,10))
-            allocate(b(1,10))
-            allocate(c(10,1))
-            allocate(xk(10,1))
-            A = reshape([1.0, -1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, & 
-                -1.0, 2.0, -1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, &
-                0.0, -1.0, 3.0, -1.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, &
-                0.0, 0.0, -1.0, 4.0, -1.0, 0.0, 0.0, 0.0, 3.0, 0.0, &
-                0.0, 0.0, 0.0, -1.0, 5.0, -1.0, 0.0, 0.0, 0.0, 0.0, &
-                0.0, 0.0, 0.0, 0.0, -1.0, 6.0, -1.0, 0.0, 0.0, 0.0, &
-                0.0, 0.0, 8.0, 0.0, 0.0, -1.0, 7.0, -1.0, 0.0, 0.0, &
-                0.0, 0.0, 0.0, 7.0, 0.0, 0.0, -1.0, 8.0, -1.0, -2.0, &
-                0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -1.0, 9.0, -1.0, &
-                1.0, 0.0, 0.0, 5.0, 0.0, 0.0, 0.0, 0.0, -1.0, 10.0], &    
-                [10, 10]) 
-            b = reshape([1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0], [1,10])
-            c = reshape([1.0, -1.0, 1.0, -1.0, 1.0, -1.0, 1.0, -1.0, 1.0, -1.0],[10,1])
-            xk = reshape([1, 1/2, 1/3, 1/4, 1/5, 1/6, 1/7, 1/8, 1/9, 1/10],[10,1])
-
-            case('G')
+            case('E') ! trivial case
             allocate(A(10,20))
             allocate(b(1,10))
             allocate(c(20,1))
@@ -121,43 +79,42 @@ program affineScaling
                 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0], &    
                 [10, 20]) 
                 b = reshape([1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0], [1,10])
-                c = reshape([1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, &
+                c = reshape([1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0, &
                              0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],[20,1])
-                xk = reshape([1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, &
+                xk = reshape([.5, 1.5, 2.5, 3.5, 4.5, 5.5, 6.5, 7.5, 8.5, 9.5, &
                               1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],[20,1])
 
-
-        case default
+        case default ! if input is not valid, retry
             write(*,*) 'Please enter a valid test case selection.'
-            write(*,*) 'Please enter a test case as "A", "B", "C", "D", or "E": '
+            write(*,'(a)', advance = 'no') 'Please enter a test case as "A", "B", "C", "D", or "E":     '
             read(*,*, iostat=stat) testChoice
-            go to 1
+            go to 1 ! do not pass go! go back and try the switch case again
 
     end select
     
-    write(*,*) 'Please enter a step size between 0 and 1 exclusive (reccomended value is .1): '
+    write(*,'(a)', advance = 'no') 'Please enter a step size between 0 and 1:   '
     read(*,*, iostat=stat) stepSize
     
     do while ((stat .ne. 0) .or. (stepSize <= 0) .or. (stepSize >= 1))      
         write(*,*) 'Please input a number from 0 to 1 exclusive. Retry.'
-        write(*,*) 'Please enter a step size between 0 and 1 (reccomended value is .1): '
+        write(*,'(a)', advance = 'no') 'Please enter a step size between 0 and 1:   '
         read(*,*, iostat=stat) stepSize
     end do
 
-    tolerance = 10.0**(-6)
+    tolerance = 10.0**(-6) ! pick stopping condition
 
-    allocate(D(size(xk),size(xk)))
+    allocate(D(size(xk),size(xk))) ! allocate other stuff based on test case
     allocate(dk(size(xk),1))
     allocate(r(size(xk),1))
     allocate(vk(size(xk),1))
     allocate(w(size(A, dim=1),1))
     allocate(oneVec(1,size(A, dim=2)))
 
-    call cpu_time(start)
-    itNum = 1000
-    do i = 1,itNum ! can change this later
+    call cpu_time(start) ! start cpu timer
+    itNum = 1000 ! can change this to allow for more iterations
+    do i = 1,itNum 
+        
         ! Step 1: Start an an interior feasible point
-        !vk = b - A*xk
         D = diagonalMatrix(xk,size(xk)) 
         
         ! Step 2: Transform to new space using affine-scaling
@@ -166,32 +123,31 @@ program affineScaling
        
         ! 2b. Compute vector of reduced costs
         call computeVectorOfReducedCosts(A, c, w, r)
-        ! 2c. Check optimality
         
+        ! 2c. Check optimality
         posR = .true.
-
         do j = 1,size(r)
-            if (r(j,1) > 0) then
+            if (r(j,1) > 0) then ! if all element is positive, r>0
                 continue
-            else
+            else ! if one element is negative, r is not >0
                 posR = .false.
             end if
         end do
 
-        temp = MATMUL(oneVec,MATMUL(D,r))
-        
-        if ((posR .and. (temp(1,1)) < tolerance)) then 
+        check = MATMUL(oneVec,MATMUL(D,r)) ! tolerence check  
+
+        if ((posR .and. (check(1,1)) < tolerance)) then ! if true, primal optimal value has been found
             write(*, *) 'Primal optimal value: ', xk 
-            call cpu_time(finish)
-            write(*,*) 'Code took ', finish - start, 'seconds with ', i, ' iterations.'
+            call cpu_time(finish) ! end timer
+            write(*,*) 'Code took ', finish - start, 'and', i, ' iterations.'
             stop
         end if
 
         ! Step 3: Compute steepest-descent direction
         call computeSteepestDescentDirection(D, r, dk)
-        ! get the conditions
+    
+        ! Step 4: Check for unbounded and constant objective value
         posdk = .true.;
-
         do j = 1,size(dk)
             if (dk(j,1) < 0) then
                 continue
@@ -208,31 +164,31 @@ program affineScaling
                 zerodk = .false.
             end if
         end do
-       
-        ! Step 4: Check for unbounded and constant objective value
-        if (posdk) then 
+
+        if (posdk) then ! if direction is positive, the problem is unbounded
             write(*,*)"This problem is unbounded"
             stop
-        else if (zerodk) then 
+        else if (zerodk) then ! if direction is close to 0, primal optimal value has been found
             write(*,*)"Primal Optimal value: ",xk
             call cpu_time(finish)
-            write(*,*) 'Code took ', finish - start, 'seconds with ', i, ' iterations.'
+            write(*,*) 'Code took ', finish - start, 'and', i, ' iterations.'
             stop
         end if
-
         ! ^ if the above conditions are unsatisfied, go to the next step
 
         ! Step 5: Perform the translation
         call computeTranslation(xk, stepSize, dk, D)
-        if (i == itNum) then
-            write(*,*) "Primal Optimal value: ", xk
+
+        if (i == itNum) then ! if maximum iterations are reached, print xk
+            write(*,*) "xk is: ", xk
             call cpu_time(finish)
-            write(*,*) 'Code took ', finish - start, 'seconds with ', itNum, ' iterations.'
+            write(*,*) 'Code took ', finish - start, 'and', i, ' iterations.'
+            write(*,*) ' iterations. The maximum iteration number was reached.'
             stop
         end if
-    end do
+    end do 
 
-    deallocate(A)
+    deallocate(A) ! deallocate all vectors
     deallocate(b)
     deallocate(c)
     deallocate(xk)
@@ -240,6 +196,7 @@ program affineScaling
     deallocate(D)
     deallocate(dk)
     deallocate(r)
+    deallocate(w)
 
     contains
         subroutine computeVectorOfDualEstimates(A,D,c,w)
@@ -251,60 +208,14 @@ program affineScaling
             real, dimension(size(A, dim=1),size(A, dim=1)) :: F
             real, dimension(size(A, dim=1),1) :: bInv
             real, dimension(size(A, dim=1),1), intent(out) :: w
-            
+            ! computes (AD(AD)^T)w = AD^2c for w 
             H = MATMUL(A, D)
             F = MATMUL(H, TRANSPOSE(H))
             bInv = MATMUL(H, MATMUL(D, c))
-            w = conjugateGradient(F,bInv)
+            w = conjugateGradient(F,bInv) ! uses the conjugate gradient numerical system solver
         end subroutine computeVectorOfDualEstimates
 
-        subroutine computeVectorOfReducedCosts(A, c, w, r)
-            implicit none
-            real, dimension(:,:), intent(in) :: A
-            real, dimension(:,:), intent(in) :: c
-            real, dimension(:,:), intent(in) :: w
-            real, dimension(size(A), 1), intent(out) :: r
-            
-            r = c - MATMUL(transpose(A), w)
-
-        end subroutine computeVectorOfReducedCosts
-
-        subroutine computeSteepestDescentDirection(D, r, dk)
-            implicit none
-            real, dimension(:,:), intent(in) :: D
-            real, dimension(:,:), intent(in) :: r
-            real, dimension(:,:), intent(out) :: dk
-        
-            dk = MATMUL(-1*D, r)
-            !write(*,*) dk
-        end subroutine computeSteepestDescentDirection
-
-        subroutine computeTranslation(xk, stepSize, dk, D)
-            implicit none
-            real, dimension(:,:), intent(inout) :: xk, dk
-            real, intent(in) :: stepSize
-            real :: mu
-            real, dimension(:,:), intent(in) :: D
-
-            mu = stepSize/maxval(-dk)
-            xk = xk + mu*MATMUL(D,dk)
-            !write(*,*) beta
-            !xk = xk - stepSize*MATMUL(D**2,dk)/norm2(MATMUL(D,dk))
-
-        end subroutine computeTranslation
-
-        function diagonalMatrix(v,n) result(D)
-            implicit none
-            integer :: n
-            real, intent(in) :: v(:,:)
-            real :: D(n,n)
-            integer :: i
-            D = 0.0
-            do i = 1, n
-                D(i,i) = v(i,1)
-            end do
-        end function
-
+        ! computes the conjugate gradient
         function conjugateGradient(F, b) result(wFinal)
             implicit none
             real, intent(in) :: F(:,:)
@@ -333,17 +244,49 @@ program affineScaling
             wFinal = reshape(w, [size(w),1])
         end function conjugateGradient
 
-        integer function displayMatrix(D, m, n)
+        subroutine computeVectorOfReducedCosts(A, c, w, r)
             implicit none
-            integer, intent(in) :: n, m
-            integer :: i, j  
-            real, dimension(:,:) :: D
-            do i = 1, m 
-                write(*, '(F8.2," ")') (D(i,j), j = 1, n) 
+            real, dimension(:,:), intent(in) :: A
+            real, dimension(:,:), intent(in) :: c
+            real, dimension(:,:), intent(in) :: w
+            real, dimension(size(A), 1), intent(out) :: r
+            ! computes r = c - A^Tw
+            r = c - MATMUL(transpose(A), w)
+        end subroutine computeVectorOfReducedCosts
+
+        subroutine computeSteepestDescentDirection(D, r, dk)
+            implicit none
+            real, dimension(:,:), intent(in) :: D
+            real, dimension(:,:), intent(in) :: r
+            real, dimension(:,:), intent(out) :: dk
+            ! computes -Dr
+            dk = MATMUL(-1*D, r)
+        end subroutine computeSteepestDescentDirection
+
+        subroutine computeTranslation(xk, stepSize, dk, D)
+            implicit none
+            real, dimension(:,:), intent(inout) :: xk, dk
+            real, intent(in) :: stepSize
+            real :: mu
+            real, dimension(:,:), intent(in) :: D
+            ! computes steplength mu = stepsize/max(-dk)
+            ! formula from source https://homepages.rpi.edu/~mitchj/handouts/interior_html/interior.html
+            mu = stepSize/maxval(-dk)
+            ! translate xk with xk+1 = xk + muDdk
+            xk = xk + mu*MATMUL(D,dk)
+        end subroutine computeTranslation
+
+        ! Helper Function
+        ! creates a matrix D with elements of xk on the diagonal
+        function diagonalMatrix(v,n) result(D)
+            implicit none
+            integer :: n
+            real, intent(in) :: v(:,:)
+            real :: D(n,n)
+            integer :: i
+            D = 0.0
+            do i = 1, n
+                D(i,i) = v(i,1)
             end do
-            displayMatrix = 1
-        end function displayMatrix
-
+        end function
 end program affineScaling
-
-! use slides and reference https://www.ise.ncsu.edu/fuzzy-neural/wp-content/uploads/sites/9/2019/10/chapter7.pdf
